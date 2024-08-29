36ea5068263ca8e52b2b30316d4442946234c7b9
Shikaruuuu <horuzou17@gmail.com>
2024-08-30 05:19:29 +0900

    design vocalMusicLesson

---
+++ frontend/src/components/vocalMusicLesson/VocalMusicLesson.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import "./VocalMusicLesson.css";
+
+const VocalMusicLesson = () => {
+  return (
+    <>
+      <div className="celloLesson">
+        <div className="celloLessonTitle">
+          <span className="celloTeacherName">声楽教室：講師　横森明美</span>
+          <img src="./akemi_profile.jpg" className="toruImg"></img>
+        </div>
+        <div className="celloDescriptionUpper">
+          <span className="celloDescriptionUpperText">
+            小学生からシニアまで『楽しく歌う』をモットーに基本的な発声を学び、イタリア歌曲から日本歌曲、叙情歌や童謡等幅広いジャンルの曲を楽しく歌える様お手伝いさせて頂きます。
+          </span>
+          <span className="celloDescriptionUpperText">
+            また「カラオケは好きだけど歌曲はちょっと」と言う方にはカラオケにも役に立つ『ヴォイストレーニング』をお勧め致します。
+          </span>
+          <span className="celloDescriptionUpperText">
+            レッスンは基本的には月謝制月2回、若しくは生徒さんのご都合に合わせたレッスン毎に次回レッスン日を決める、１レッスン制で行います。
+          </span>
+        </div>
+        <div className="celloDescriptionMiddle">
+          <div className="priceTitle">
+            <span className="priceTitleText">
+              声楽レッスン料金　レッスン料金
+            </span>
+          </div>
+          <ul>
+            <li>月謝制月２回</li>
+            <ul>
+              <li>初級：30分　2回　 9,000円</li>
+              <li>中級：40分　2回　11,000円</li>
+              <li>上級：50分　2回　13,000円</li>
+            </ul>
+            <li>１レッスン制</li>
+            <ul>
+              <li>初級：30分　1回　 5,000円</li>
+              <li>中級：40分　1回　 6,000円</li>
+              <li>上級：50分　1回　 7,000円</li>
+            </ul>
+          </ul>
+        </div>
+        <div className="celloDescriptionBottom">
+          <span className="celloDescriptionBottomText">
+            ご興味ございましたらお気軽にお問い合わせフォームよりお問い合わせ下さい。
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VocalMusicLesson;
--- frontend/src/pages/vocalLesson/VocalLesson.jsx
+++ frontend/src/pages/vocalLesson/VocalLesson.jsx
@@ -1,61 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./VocalLesson.css";
-import Header from '../../components/header/Header';
-import Navbar from '../../components/navbar/Navbar';
-import Hamburger from '../../components/hamburger/Hamburger';
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import Hamburger from "../../components/hamburger/Hamburger";
+import VocalMusicLesson from "../../components/vocalMusicLesson/VocalMusicLesson";
 
 const VocalLesson = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    // ウィンドウサイズが変更された時にサイズを更新する
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
+  // ウィンドウサイズが変更された時にサイズを更新する
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
 
-        window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
-        // コンポーネントがアンマウントされた際にイベントリスナーを削除
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-    return(
-        <>
-        <Header />
-        {/* 768px未満のときのみNavbar（ハンバーガーメニュー）を表示 */}
-        {windowWidth > 768 ? <Navbar /> : <Hamburger />}
-        <div className="music-lesson">
-
-    <h2>声楽教室：講師　横森明美</h2>
-    <p>
-      小学生からシニアまで『楽しく歌う』をモットーに基本的な発声を学び、イタリア歌曲から日本歌曲、叙情歌や童謡等幅広いジャンルの曲を楽しく歌える様お手伝いさせて頂きます。
-      また「カラオケは好きだけど歌曲はちょっと」と言う方にはカラオケにも役に立つ『ヴォイストレーニング』をお勧め致します。
-    </p>
-    <p>
-      レッスンは基本的には月謝制月2回、若しくは生徒さんのご都合に合わせたレッスン毎に次回レッスン日を決める、１レッスン制で行います。
-    </p>
-    <h3>声楽レッスン料金（税込）</h3>
-    <ul>
-      <li>月謝制月２回</li>
-      <ul>
-        <li>初級：30分　2回　 9,000円</li>
-        <li>中級：40分　2回　11,000円</li>
-        <li>上級：50分　2回　13,000円</li>
-      </ul>
-      <li>１レッスン制</li>
-      <ul>
-        <li>初級：30分　1回　 5,000円</li>
-        <li>中級：40分　1回　 6,000円</li>
-        <li>上級：50分　1回　 7,000円</li>
-      </ul>
-    </ul>
-    <p>
-      歌を歌う・声を出す、と言った行動は日頃のストレス発散や健康増進にもとても役に立つと共に、歌う事により『幸せホルモン』が分泌され『幸福感』や『満足感』を感じやすくなると言われております。お気軽にお問い合わせフォームよりお問い合わせ下さい。
-    </p>
-  </div>
-  </>
-    )
-}
+    // コンポーネントがアンマウントされた際にイベントリスナーを削除
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <>
+      <Header />
+      {/* 768px未満のときのみNavbar（ハンバーガーメニュー）を表示 */}
+      {windowWidth > 768 ? <Navbar /> : <Hamburger />}
+      <VocalMusicLesson />
+    </>
+  );
+};
 
 export default VocalLesson;
