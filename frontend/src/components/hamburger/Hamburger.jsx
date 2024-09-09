ca912a01ffd9d0c7d1bb700e6e7419c1b41090ed
Shikaruuuu <horuzou17@gmail.com>
2024-09-10 01:25:26 +0900

    change menu order

--- frontend/src/components/hamburger/Hamburger.jsx
+++ frontend/src/components/hamburger/Hamburger.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Hamburger.css";
 
 export default function Hamburger() {
@@ -15,48 +15,75 @@ export default function Hamburger() {
       {isOpen && (
         <ul className="hamburger-items">
           <li className="navbarListItem">
-            <Link to="/" className="navbarLink" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">ホーム</span>
+            <Link
+              to="/"
+              className="navbarLink"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">ホーム</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/vocalLesson" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">声楽教室</span>
+            <Link
+              to="/musiclesson"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">チェロ教室</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/musiclesson" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">チェロ教室</span>
+            <Link
+              to="/vocalLesson"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">声楽教室</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/concert" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">出張演奏</span>
+            <Link
+              to="/concert"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">出張演奏</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/profile" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">プロフィール</span>
+            <Link
+              to="/profile"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">プロフィール</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/faq" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">お問い合わせ</span>
+            <Link
+              to="/access"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">アクセス</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/access" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">アクセス</span>
+            <Link
+              to="/faq"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">お問い合わせ</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/shop" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">提携工房</span>
+            <Link
+              to="/blog"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">ブログ</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/blog" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">ブログ</span>
+            <Link
+              to="/shop"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">提携工房</span>
             </Link>
           </li>
         </ul>
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,61 +1,86 @@
-import React from 'react';
+import React from "react";
 import "./Navbar.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  
   return (
     <div className="navbar">
       <div className="navbarWrapper">
         <ul className="navbarList">
-        <li className="navbarListItem">
-            <Link to="/" className="navbarLink" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">ホーム</span>
+          <li className="navbarListItem">
+            <Link
+              to="/"
+              className="navbarLink"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">ホーム</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/vocalLesson" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">声楽教室</span>
+            <Link
+              to="/musiclesson"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">チェロ教室</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/musiclesson" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">チェロ教室</span>
+            <Link
+              to="/vocalLesson"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">声楽教室</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/concert" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">出張演奏</span>
+            <Link
+              to="/concert"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">出張演奏</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/profile" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">プロフィール</span>
+            <Link
+              to="/profile"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">プロフィール</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/faq" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">お問い合わせ</span>
+            <Link
+              to="/access"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">アクセス</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/access" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">アクセス</span>
+            <Link
+              to="/faq"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">お問い合わせ</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/shop" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">提携工房</span>
+            <Link
+              to="/blog"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">ブログ</span>
             </Link>
           </li>
           <li className="navbarListItem">
-            <Link to="/blog" className="navbarListItemText" style={{ textDecoration: "none", color: "black" }}>
-            <span className="navbarListItemText">ブログ</span>
+            <Link
+              to="/shop"
+              className="navbarListItemText"
+              style={{ textDecoration: "none", color: "black" }}>
+              <span className="navbarListItemText">提携工房</span>
             </Link>
           </li>
-
         </ul>
       </div>
     </div>
-  )
+  );
 }
