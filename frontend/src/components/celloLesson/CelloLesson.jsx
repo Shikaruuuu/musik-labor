55abf8cf5f65bdbe8b2a3a8bfbc8881407583048
Shikaruuuu <horuzou17@gmail.com>
2024-09-10 02:23:44 +0900

    update cello Lesson page

--- frontend/src/components/celloLesson/CelloLesson.jsx
+++ frontend/src/components/celloLesson/CelloLesson.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import "./CelloLesson.css";
 
 const CelloLesson = () => {
@@ -11,43 +10,181 @@ const CelloLesson = () => {
         </div>
         <div className="celloDescriptionUpper">
           <span className="celloDescriptionUpperText">
-            人間の声に一番近い楽器と言われるチェロ。その豊かな響きをお子様からシニアまで、幅広い年齢層の方々に『チェロを奏でる歓び』を体験して頂ける様、初めて楽器を構える基礎の基礎から「白鳥」や「バッハ無伴奏チェロ組曲」等のチェロの名曲を奏でられる様になるまでの全てをお手伝いさせて頂きます。またチェロは弦楽器の中で最も自然体で構える事が出来る楽器としても、体に無理なく演奏出来る所がお勧めのポイントです。
+            人間の声に一番近い楽器と言われるチェロ。その豊かな響きをお子様からシニアまで、幅広い年齢層の方々に『チェロを奏でる歓び』を体験して頂ける様、
           </span>
           <span className="celloDescriptionUpperText">
-            またチェロ教室の特徴の一つとして『チェロアンサンブル』を取り入れております。
+            初めて楽器を構える基礎の基礎から「白鳥」や「バッハ無伴奏チェロ組曲」等のチェロの名曲を奏でられる様になるまでの全てをお手伝いさせて頂きます。
           </span>
           <span className="celloDescriptionUpperText">
-            『チェロアンサンブル』とは通常のレッスンでは生徒さんと講師の二重奏で行い、生徒さんは講師の音を聴きながら音程やリズムを合わせたり、生徒さんのソロに講師が伴奏を付けると言った合奏を行います。
+            またチェロは弦楽器の中で最も自然体で構える事が出来る楽器としても、体に無理なく演奏出来る所がお勧めのポイントです。
           </span>
           <span className="celloDescriptionUpperText">
-            その他、年に1回～2回程度音楽ホールにおいて合同ホールレッスン及び、チェロ四重奏等のアンサンブルレッスンも行っております。
+            なお当教室では『サイレントチェロ』でのレッスンも行っております。
           </span>
           <span className="celloDescriptionUpperText">
-            この『チェロアンサンブル』がとても好評でみなさん大変喜ばれております。
+            サイレントチェロでのレッスンは、基本的にはギターアンプを使い実際に音
+            を出してのレッスンを行います。(生徒さんのご自宅等で音出しが出来ない
+            場合を除く)
           </span>
         </div>
         <div className="celloDescriptionMiddle">
+          <div className="lessonDescriptionWrapper">
+            <span className="lessonDescriptionTitle">1レッスン制</span>
+            <span className="lessonDescription">
+              レッスンは基本的にレッスン毎に次回のレッスン日を決める『1レッスン制』
+              を採用しております。
+            </span>
+            <span className="lessonDescription">
+              生徒さんのご都合・ご予定に柔軟に対応すると共に、講師もチェリストとし
+              て演奏活動を行っている為、
+            </span>
+            <span className="lessonDescription">
+              コンサートへの出演等で予定が不規則になって
+              しまう場合がある為です。この点だけご理解ご了承下さい。
+            </span>
+          </div>
           <div className="priceTitle">
+            <span className="priceTitleText">チェロレッスン料金</span>
             <span className="priceTitleText">
-              チェロ・サイレントチェロ　レッスン料金
+              (チェロ・サイレントチェロ共に)
             </span>
           </div>
           <ul>
-            <li>通常レッスン：1レッスン（生徒さんにお越し頂く場合）</li>
             <ul>
-              <li>大人：75分　 11,000円（税込）</li>
-              <li>高校生以下：45分　 7,000円（税込）</li>
-            </ul>
-            <li>出張レッスン：1レッスン神奈川県内（県外は別途ご相談下さい）</li>
-            <ul>
-              <li>大人：80分　 15,000円～（税・出張費込）</li>
-              <li>高校生以下：50分　 11,000円～（税・出張費込）</li>
+              <li className="priceList">大人：75分　11,000円（税込）</li>
+              <li className="priceList">高校生以下：45分　7,000円（税込）</li>
             </ul>
           </ul>
+          <div className="trialLesson">
+            <span className="trialLessonDescription">
+              *体験レッスン:30 分 1,000 円
+            </span>
+            <span className="trialLessonDescription">
+              (レッスンを行う上で生徒さん
+              と講師のコミュニケーションはとても重要と考えます。
+            </span>
+            <span className="trialLessonDescription">
+              生徒さんから見ても講師と合う合わないは当然有ると思いますので、是非レッスンを体験してみて下さい)
+            </span>
+          </div>
+        </div>
+        <div className="celloDescriptionMiddle">
+          <div className="lessonDescriptionWrapper">
+            <span className="lessonDescriptionTitle">出張チェロ教室</span>
+            <span className="lessonDescription">
+              チェロ教室では様々なご要望にお応えする為に『出張チェロ教室』も開校し
+              ております。
+            </span>
+            <span className="lessonDescription">
+              生徒さんのご自宅や、ご自宅近くのレンタルスペース・コミュニティセンタ
+              ー・貸しスタジオ等、
+            </span>
+            <span className="lessonDescription">
+              生徒さんの希望される場合へ講師が出張してレッスン
+              を行います。(レッスン場所は生徒さんご自身でご用意をお願い致します)
+            </span>
+          </div>
+          <div className="priceTitle">
+            <span className="priceTitleText">出張チェロレッスン料金</span>
+            <span className="priceTitleText">
+              (チェロ・サイレントチェロ共に)
+            </span>
+          </div>
+          <ul>
+            <ul>
+              <li className="priceList">
+                大人：80分　15,000円〜（税・出張費込）
+              </li>
+              <li className="priceList">
+                高校生以下：50分　11,000円〜（税・出張費込）
+              </li>
+            </ul>
+            <span>
+              上記は神奈川県内の出張料金です。県外は別途ご相談下さい。
+            </span>
+          </ul>
+        </div>
+        <div className="lessonContents">
+          <div className="LessonConttentsTitle">
+            <span className="LessonContentsTitleText">レッスン内容</span>
+            <span className="LessonContentsTitleSubText">
+              （チェロレッスン・出張チェロレッスン共通）
+            </span>
+          </div>
+          <div className="LessonContentsDesctiptionUpper">
+            <span className="LessonContentsDesctiptionText">
+              初めてチェロを習う方にはチェロの構え方や弓の持ち方、「ウェルナーチェロ教則本」や
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              様々なメソッド等を使いながら小さな曲を少しずつ弾き進めて行き、
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              基本的なボーイング、フィンガリングを覚えた比較的早い段階で講師との二重奏、
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              アンサンブルを取り入れたレッスンを楽しんで頂きます。
+            </span>
+          </div>
+          <div className="LessonContentsDesctiptionMiddle">
+            <span className="LessonContentsDesctiptionText">
+              過去にチェロを習っていたご経験のある方や、レッスンは未経験ながらも独学で
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              チェロを弾いていらっしゃる方には、ご自身でお持ちの教則本や現在弾いている楽譜
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              をお持ちになって頂き、レッスンを行いながら最適と思われる
+              教材や楽曲、講師との二重奏曲のご提案をさせて頂きます。
+            </span>
+          </div>
+          <div className="LessonContentsDesctiptionBottom">
+            <span className="LessonContentsDesctiptionText">
+              生徒さんご自身もチェロを演奏する上で弾きたい曲や挑戦してみたい曲等お有りになると思いますので、
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              生徒さんとコミュニケーションを取りながら極力ご意向に沿える様お手伝いさせて頂きます。
+            </span>
+          </div>
+        </div>
+        <div className="lessonContents">
+          <div className="LessonConttentsTitle">
+            <span className="LessonContentsTitleText">ホールレッスン</span>
+          </div>
+          <div className="LessonContentsDesctiptionUpper">
+            <span className="LessonContentsDesctiptionText">
+              普段ご自宅やコミュニティセンター等、本来クラシック音楽を奏でる為の環境では無い響きでの練習やレッスンを行うのに対し、
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              生徒さんそれぞれお持ちの楽器の響きに音楽ホールの持つ独特の響き、音色、残響、空間をプラスし、生徒さん皆さんがコンサートホールでのチェロの響きを堪能する事が出来る、
+              音楽ホール(旭区民文化センターサンハート)での合同レッスン練習会を約半年毎に開催しております。
+            </span>
+          </div>
+          <div className="LessonContentsDesctiptionMiddle">
+            <span className="LessonContentsDesctiptionText">
+              合同レッスン練習会では個人レッスンとチェロアンサンブルを行います。
+            </span>
+          </div>
+          <div className="LessonContentsDesctiptionBottom">
+            <span className="LessonContentsDesctiptionText">
+              普段はご自宅や、コミュニティセンター、貸スタジオ等の限られたスペースでの演奏になりますが、本来チェロはとても豊かな響きを持った楽器であり、
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              その楽器の響きを最大限に活かすのがコンサートホールでの演奏です。(私も33年間在籍していた神奈川フィルハーモニー管弦楽団で全国のホールで
+            </span>
+            <span className="LessonContentsDesctiptionText">
+              演奏致しましたが、本当にホールの響きによってオーケストラの鳴り方や音色も変わってしまう位重要な要素でした。正にホールも楽器の一部です)
+            </span>
+          </div>
         </div>
         <div className="celloDescriptionBottom">
           <span className="celloDescriptionBottomText">
-            ご興味ございましたらお気軽にお問い合わせフォームよりお問い合わせ下さい。
+            *なおサイレントチェロは音楽ホールでのアンプ使用が禁止されている為、
+            こちらでご用意するチェロをお使い頂きます。
+          </span>
+          <span className="celloDescriptionBottomText">
+            チェロを弾いてみたい、サイレントチェロでレッスンを受けたい、自宅や近
+            所でレッスンを受けたい、その他ご希望やご要望がございましたら『お問い
+            合わせフォーム』よりお問い合わせ下さい。
           </span>
         </div>
       </div>
