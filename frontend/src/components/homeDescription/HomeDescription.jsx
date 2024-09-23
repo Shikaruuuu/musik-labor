36cc3751796a8803b0652da6838c8324ab335614
Shikaruuuu <horuzou17@gmail.com>
2024-09-24 00:57:15 +0900

    rewrite home description

--- frontend/src/components/homeDescription/HomeDescription.jsx
+++ frontend/src/components/homeDescription/HomeDescription.jsx
@@ -1,25 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./HomeDescription.css";
 
 export default function HomeDescription() {
   return (
     <>
       <div className="HomeDescription">
-        <span className="homeDescriptionText">
-          『ムジーク・らぼあ』では子供から大人、
-        </span>
-        <span className="homeDescriptionText">
-          シニアまで全ての方に音楽の 楽しさ、素晴らしさを感じて頂ける様、
-        </span>
-        <span className="homeDescriptionText">
-          元神奈川フィルハーモニー管弦楽団チェロ奏者横森徹と二期会オペラ歌手横森明美が、
-        </span>
-        <span className="homeDescriptionText">
-          チェロ教室・声楽教室をはじめ、高齢者施設や各種パーティー会場等への出張演奏・
-        </span>
-        <span className="homeDescriptionText">
-          トレーナー・楽器購入のお手伝いまで幅広くご提案させて頂いております。
-        </span>
+        <span className="homeDescriptionText">プロチェリストと</span>
+        <span className="homeDescriptionText">オペラ歌手による</span>
+        <span className="homeDescriptionText">横浜発の音楽教室</span>
       </div>
     </>
   );
